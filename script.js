87c2b7b49b119c92c5850e96398f9f8f578f9a4c
”0xAureoleNode“ <aureolenode@gmail.com>
2022-12-28 21:16:06 +0800

    oop

--- script.js
+++ script.js
@@ -4,4 +4,26 @@ window.addEventListener('load', function () {
   const ctx = canvas.getContext('2d');
   canvas.width = 1500;
   canvas.height = 500;
+
+  // keep track of specified user input
+  class InputHandler {}
+  class Projectile {}
+
+  // deal with falling screws, corks and bolts that come from damaged enemies
+  class Particle {}
+
+  // control the main character
+  class Player {}
+
+  // main blueprint handling many different enemy types
+  class Enemy {}
+
+  class Layer {}
+
+  class Background {}
+
+  class UI {}
+
+  // brain of project
+  class Game {}
 });
